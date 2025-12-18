3434ad7f41d86798b5aca46cceb3a759b0e3df06
wagnificent <91030663+wagnificent@users.noreply.github.com>
2025-12-18 13:13:38 -0500

    Cleaned out old and unused files

--- check-teams.js
+++
@@ -1,8 +0,0 @@
-const { SupabaseManager } = require('./supabase-api-manager');
-
-(async () => {
-    const mgr = new SupabaseManager();
-    const teams = await mgr.getTeams();
-    console.log('Teams in database:');
-    teams.forEach(t => console.log(`${t.id}: ${t.name}`));
-})();
